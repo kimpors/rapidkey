fd674532b2d816db25ce122a821eb5bbef4d25cf
kimpors <mikita_korshak@ukr.net>
2024-12-27 14:34:24 +0200

    Create basic version

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include "cmdio.h"
 
+static char *sbuf = "this is a test";
+
 int main(void)
 {
-	while (1)
+	char *ps = sbuf;
+	size_t row = 1, col = 1;
+
+	coclear();
+	printf("%s", ps);
+	comove(row, col);
+
+
+	int ch;
+	while (*ps)
 	{
 		if (cokbhit())
 		{
-			printf("ch: %c\n", cogetch());
+			ch = cogetch();
+			if (ch == '\b' && ps - sbuf > 0)
+			{
+				printf("\033[0m");
+				putchar(*(ps--));
+				comove(row, --col);
+				continue;
+			}
+
+			ch == *ps
+				? printf("\033[32m")
+				: printf("\033[31m");
+
+			putchar(*(ps++));
+			comove(row, ++col);
 		}
 	}
+
+	printf("\033[0m");
 	return 0;
 }
