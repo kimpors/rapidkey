e2dd9d9e61c65fdf8360b837421849b972795b48
kimpors <mikita_korshak@ukr.net>
2025-03-30 14:48:43 +0300

    Add cli template
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- main.c
+++ main.c
@@ -1,13 +1,61 @@
 #include <stdio.h>
 #include "cmdio.h"
 #include <stdlib.h>
+#include <string.h>
 
 int pop(void);
 void push(size_t val);
 static char *sbuf = "this is a test\nthis is also test\nAnd this is also test";
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int r, c;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "generate") == 0)
+		{
+			i = i < argc ? i + 1 : i;
+
+			if ( strcmp(argv[i], "numbers") == 0)
+			{
+				printf("numbers generator\n");
+			}
+			else if (strcmp(argv[i], "symbols") == 0)
+			{
+				printf("symbols generator\n");
+			}
+			else if (strcmp(argv[i], "words") == 0)
+			{
+				printf("words generator\n");
+			}
+			else if (strcmp(argv[i], "mixed") == 0)
+			{
+				printf("mixed generator\n");
+			}
+
+			if (!(i++ < argc - 1)) break;
+
+			if (sscanf(argv[i], "%dx%d", &r, &c))
+			{
+				printf("row: %d\n", r);
+				printf("col: %d\n", r);
+			}
+		}
+		else if (strcmp(argv[i], "help") == 0)
+		{
+			printf("help page\n");
+			break;
+		}
+		else
+		{
+			fprintf(stderr, "wrong argument\n");
+			exit(-1);
+		}
+	}
+
+	return 0;
+
 	char *ps = sbuf;
 	size_t row = 1, col = 1;
 
